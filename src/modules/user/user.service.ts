cb5e4e84711489211b3feb4bf2b7e5218df934e4
TommyH <hiept81331@gmail.com>
2025-08-03 21:07:36 +0700

    entities movie app

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,26 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
-
-  findAll() {
-    return `This action returns all user`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+  async createUser(userData: User): Promise<User> {
+    const user = this.userRepository.create(userData);
+    user.created_at= new Date();
+    user.updated_at = new Date();
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    user.password = hashedPassword;
+    return this.userRepository.save(user);
   }
 }
